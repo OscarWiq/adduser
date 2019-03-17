1f32cd71524d926a2914d5504a9d443a86ef5f4f
Oscar <48216247+OscarWiq@users.noreply.github.com>
2019-03-17 19:17:53 +0100

    cleaned up var names

--- adduser.py
+++ adduser.py
@@ -13,14 +13,14 @@ with open(file_name, 'r') as f:
     full_name = f.read().splitlines()  # splitlines removes \n from each array element
     # names = list(map(lambda name: (name.split()[0], name.split()[1]), full_name))
     names = list(map(splitter, full_name))
-
-for (user_id, password, fname, lname) in func.generate_candidates(names):
+    
+for (user_id, password, first_name, last_name) in func.generate_candidates(names):
     cmd_command = "dsadd user cn=" + user_id + "," + ou + ",dc=g105,dc=local -memberof "
     cmd_command += "cn=" + group + "," + ou + ",dc=g105,dc=local "
     cmd_command += " -samid " + user_id + "-upn " + user_id
-    cmd_command += " -fn " + fname + " -ln " + lname + " -hmdir " + unc
+    cmd_command += " -fn " + first_name + " -ln " + last_name + " -hmdir " + unc
     cmd_command += " -hmdrv H -pwd " + password + " -pwdneverexpires no"
-    func.run_cmd(cmd_command)
+    print(cmd_command)
 
 input("Press ENTER to exit")
 
