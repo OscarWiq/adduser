c22642b985ccaeca543afd24a8209bac6c7ccc19
Oscar <48216247+OscarWiq@users.noreply.github.com>
2019-03-17 10:21:37 +0100

    cleaned up code

--- adduser.py
+++ adduser.py
@@ -6,9 +6,9 @@ unc = input("UNC-sökväg till mappen med hemkataloger: ")
 group = input("Namn på gruppen som kontona ska tillhöra: ")
 
 with open(file_name, 'r') as f:
-    name = f.read().splitlines()  # splitlines removes /n from each array element
-    first_name = [x.split()[0] for x in name]
-    last_name = [y.split()[1] for y in name]
+    full_name = f.read().splitlines()  # splitlines removes \n from each array element
+    first_name = [x.split()[0] for x in full_name]
+    last_name = [y.split()[1] for y in full_name]
 
 for first_names, last_names in zip(first_name, last_name):
     func.generate_id(first_names, last_names)
