2b70333315c32aa60742800bca1117a72d02dc9e
Oscar <48216247+OscarWiq@users.noreply.github.com>
2019-03-17 19:24:30 +0100

    added lamba function as main method of splitting
    looks cleaner without splitter function at very little cost of performance

--- adduser.py
+++ adduser.py
@@ -5,15 +5,10 @@ ou = input("OU som användarkontona ska skapas i (RDN): ")
 unc = input("UNC-sökväg till mappen med hemkataloger: ")
 group = input("Namn på gruppen som kontona ska tillhöra: ")
 
-def splitter(str):
-    str = str.split()
-    return (str[0], str[1])
-
 with open(file_name, 'r') as f:
-    full_name = f.read().splitlines()  # splitlines removes \n from each array element
-    # names = list(map(lambda name: (name.split()[0], name.split()[1]), full_name))
-    names = list(map(splitter, full_name))
-    
+    full_names = f.read().splitlines()  # splitlines removes \n from each array element
+    names = list(map(lambda name: (name.split()[0], name.split()[1]), full_names))
+
 for (user_id, password, first_name, last_name) in func.generate_candidates(names):
     cmd_command = "dsadd user cn=" + user_id + "," + ou + ",dc=g105,dc=local -memberof "
     cmd_command += "cn=" + group + "," + ou + ",dc=g105,dc=local "
