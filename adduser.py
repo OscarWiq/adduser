bbf4e0e62569daa925f95f759cd877a30c070cb5
Oscar <48216247+OscarWiq@users.noreply.github.com>
2019-03-17 20:06:25 +0100

    added func.run_cmd again

--- adduser.py
+++ adduser.py
@@ -14,7 +14,7 @@ for (user_id, password, first_name, last_name) in func.generate_candidates(names
     cmd_command += " -samid " + user_id + "-upn " + user_id
     cmd_command += " -fn " + first_name + " -ln " + last_name + " -hmdir " + unc
     cmd_command += " -hmdrv H -pwd " + password + " -pwdneverexpires no"
-    print(cmd_command)
+    func.run_cmd(cmd_command)
 
 input("Press ENTER to exit")
 
